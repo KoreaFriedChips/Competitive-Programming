ff7da3071c3c48a7f7f5eb32c091ccae56967236
KoreaFriedChips <54821783+KoreaFriedChips@users.noreply.github.com>
2021-03-16 16:50:17 -0400

    Create R2.cpp

---
+++ COCI/'06 Contest 2/R2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <cmath>
+#include <queue>
+#include <string>
+#include <bitset>
+#include <deque>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ull unsigned long long
+#define pii pair<int, int>
+#define mp make_pair
+#define vi vector<int>
+#define endl '\n'
+#define MOD 1000000007
+#define MM 53
+#define row first
+#define col second
+
+
+int main() 
+{
+   cin.sync_with_stdio(0);
+   cin.tie(0);
+   int r, s; cin >> r >> s;
+   cout << 2*s-r << endl;
+   return 0;
+}
